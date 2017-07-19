ba2bc25d0e9262b8ebfafa743c9e8fa876ad3a77
Circle CI <coldnew.tw@gmail.com>
2017-07-19 09:19:17 +0800

    Deploy commit d9358b5 to GitHub Pages: Wed Jul 19 09:19:17 CST 2017

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learning note on google-interview-university
+
+[![CircleCI](https://circleci.com/gh/coldnew/google-interview-university.svg?style=svg)](https://circleci.com/gh/coldnew/google-interview-university)
+
+<h3><a href="https://coldnew.github.io/google-interview-university">Online Version</a></h3>
+
+This is my learning note on [google-interview-university](https://github.com/jwasham/google-interview-university), I use [org-mode](http://orgmode.org) to write my note instead of markdown and all text written in `Traditional Chinese`.
+
+這是我學習 [google-interview-university](https://github.com/jwasham/google-interview-university) 所撰寫的筆記，使用 `繁體中文` 撰寫。
+
+![future-googler](https://github.com/coldnew/google-interview-university/blob/master/images/future-googler.png)
+
+## Credits
+
+The theme I used is based on [org-spec](https://github.com/thi-ng/org-spec).
+
+## License
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# exit with nonzero exit code if anything fails
+set -e
+
+# Local variables
+OUT=".gh-pages"
+ID=$(git rev-parse --short HEAD)
+DATE=$(date)
+
+# clear and re-create the out directory
+rm -rf $OUT || exit 0;
+
+# create repo directory
+mkdir -p $OUT
+
+# Copy all prebuild files
+cp README.* $OUT
+cp -R * $OUT
+cp -f circle.yml $OUT
+
+# go to the out directory and create a *new* Git repo
+cd $OUT
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Circle CI"
+git config user.email "coldnew.tw@gmail.com"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "Deploy commit $ID to GitHub Pages: $DATE"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "git@github.com:coldnew/google-interview-university.git" master:gh-pages > /dev/null 2>&1
